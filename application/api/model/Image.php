69ba33db79f67a53b8207a6339bbd186ab42515a
kalicodextu <kalicodextu@gmail.com>
2018-07-19 01:48:32 +0800

    add BaseMdel to hidden field and complete url path

--- application/api/model/Image.php
+++ application/api/model/Image.php
@@ -2,10 +2,12 @@
 
 namespace app\api\model;
 
-use think\Model;
-
 class Image extends BaseModel
 {
     //
     protected $hidden = ['delete_time', 'id', 'from'];
+    public function getUrlAttr($value, $data)
+    {
+        return $this->prefixImageUrl($value, $data);
+    }
 }
---
+++ application/extra/setting.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by kalicodextu.
+ * User: kali
+ * Date: 2018/7/17
+ * Time: 下午9:32
+ */
+return [
+    'img_prefix' => 'http://zerg.cn/images'
+];
+
