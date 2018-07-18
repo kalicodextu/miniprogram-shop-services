d2f0f8f545bade8df9d150537ddc0fbf1bfbffed
kalicodextu <kalicodextu@gmail.com>
2018-07-19 01:45:52 +0800

    add BaseMdel to hidden field and complete url path

--- application/api/model/BaseModel.php
+++ application/api/model/BaseModel.php
@@ -8,4 +8,13 @@ class BaseModel extends Model
 {
     //
     protected $hidden = ['delete_time'];
+
+    protected function prefixImageUrl($value, $data)
+    {
+        $finalUrl = $value;
+        if ($data['from'] == 1) {
+            $finalUrl = config('setting.img_prefix') . $value;
+        }
+        return $finalUrl;
+    }
 }
